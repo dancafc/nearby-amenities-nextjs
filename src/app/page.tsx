1be90ebf4bbf5e6dc004da0a3d449330a1483761
Daniel Roth <danielroth@daniels-mbp.home>
2025-02-02 20:39:44 +0000

    more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,14 @@ import styles from "./page.module.css";
 
 const Page = () => {
     const [amenities, setAmenities] = useState<IAmenity[]>([]);
-    const [selectedAmenity, setSelectedAmenity] = useState<OsmAmenityType>(OsmAmenityType.toilets);
+    const [selectedAmenity, setSelectedAmenity] = useState<IAmenity | null>(null);
+    const [selectedAmenityType, setselectedAmenityType] = useState<OsmAmenityType>(OsmAmenityType.toilets);
     const [distanceInput, setDistanceInput] = useState<number>(200);
 
     const handleFindClick: () => Promise<void> = async () => {
         const amenitiesService = new AmenitiesService();
         await amenitiesService.getCurrentLocation();
-        const results: IAmenity[] = await amenitiesService.getNearbyAmenities(selectedAmenity, distanceInput);
+        const results: IAmenity[] = await amenitiesService.getNearbyAmenities(selectedAmenityType, distanceInput);
         setAmenities(results);
     };
 
@@ -24,50 +25,67 @@ const Page = () => {
         }
     };
 
+    const handleAmenityClick = (amenity: IAmenity) => {
+        setSelectedAmenity(amenity);
+        console.log(selectedAmenity);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.card}>
                 <h1 className="text-2xl font-bold mb-4">Find Nearby Amenities</h1>
 
-                <div className={styles.selectContainer}>
+                <div className={styles.formContainer}>
                     <select
                         className={styles.selectBox}
-                        value={selectedAmenity}
-                        onChange={(e) => setSelectedAmenity(e.target.value as OsmAmenityType)}
+                        value={selectedAmenityType}
+                        onChange={(e) => setselectedAmenityType(e.target.value as OsmAmenityType)}
                     >
                         <option value={OsmAmenityType.toilets}>🚻 Toilets</option>
                         <option value={OsmAmenityType.drinking_water}>💧 Drinking Water</option>
                     </select>
-                    <button onClick={handleFindClick} className={styles.button}>Find</button>
+
                 </div>
 
-                <h1 className="text-2xl font-bold mb-4">Within (m)</h1>
-                <input
-                    type="number"
-                    value={distanceInput}
-                    onChange={(e) => setDistanceInput(Number(e.target.value))}
-                    onKeyDown={handleKeyDown}
-                    className={styles.input}
-                />
+                <h1 className="text-1xl font-bold mb-2 mt-2">Within (m)</h1>
+                <div className={styles.formContainer}>
+                    <input
+                        type="number"
+                        value={distanceInput}
+                        onChange={(e) => setDistanceInput(Number(e.target.value))}
+                        onKeyDown={handleKeyDown}
+                        className={styles.input}
+                    />
+                    <button onClick={handleFindClick} className={styles.button}>Find</button>
+                </div>
             </div>
 
             <div className={styles.amenitiesList}>
-                <h2 className="text-xl font-semibold mb-3">Results:</h2>
+            <h2 className="text-xl font-semibold mb-3">Results:</h2>
                 {amenities.length === 0 ? (
                     <p className="text-gray-500 text-center">No amenities found.</p>
                 ) : (
                     <ul>
-                        {amenities.map((amenity: IAmenity) => (
-                            <li key={`${amenity.location.lat}-${amenity.location.lon}`} className={styles.resultItem}>
-                                <p className={styles.resultText}>📍 Lat: {amenity.location.lat},
-                                    Lon: {amenity.location.lon}</p>
-                            </li>
+                        {amenities.map((amenity) => (
+                            <>
+                                <li
+                                    key={`${amenity.location.lat}-${amenity.location.lon}`}
+                                    className={styles.resultItem}
+                                    onClick={() => handleAmenityClick(amenity)}
+                                >
+                                    📍 Lat: {amenity.location.lat}, Lon: {amenity.location.lon}
+                                </li>
+
+                            </>
                         ))}
                     </ul>
                 )}
             </div>
+
         </div>
     );
+
+
 };
 
 export default Page;
